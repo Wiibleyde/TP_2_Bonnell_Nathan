32241e9f6801f934f92e8a444e39c542272844f7
Wiibleyde <nathan@bonnell.fr>
2026-03-06 10:17:50 +0100

    fix: update logger middleware to use req.originalUrl for accurate logging
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -5,7 +5,7 @@ export const logger = (req: express.Request, res: express.Response, next: expres
     res.on('finish', () => {
         const duration = Date.now() - start;
         const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
-        console.log(`[${timestamp}] ${req.method} ${req.url} - ${res.statusCode} - ${duration}ms`);
+        console.log(`[${timestamp}] ${req.method} ${req.originalUrl} - ${res.statusCode} - ${duration}ms`);
     });
     next();
 };
